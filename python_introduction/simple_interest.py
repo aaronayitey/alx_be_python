798fdceaf8e842dae55117ed11537bbf98319fb8
aaronayitey <aaronayitey@gmail.com>
2024-09-10 08:57:09 +0000

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Assign princpal, rate and time to get simple interest.
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal*rate*time # calculates interest
+
+print(f"The simple interest is: {interest}")

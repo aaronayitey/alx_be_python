57dc468d7d6ff920e7ebe02e7e18fc0fc73a698a
aaronayitey <aaronayitey@gmail.com>
2024-09-20 14:50:26 +0000

    Update pattern_drawing.py to pass tests

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,11 +2,15 @@
 
 try:
 
-    number = int(input("Enter the size of the pattern: "))
+    size = int(input("Enter the size of the pattern: "))
 
-    count = 0
-    while count < number:
-        print("*"* number, end="\n")
-        count += 1
+    row = 0
+    while row < size:
+        for a in range(size):
+            print("*", end="")
+        
+        print()
+
+        row += 1
 except ValueError:
     print("Enter numbers only!") 

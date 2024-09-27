506098a7a48d1131b494b5158c197c3c983d3506
aaronayitey <aaronayitey@gmail.com>
2024-09-27 18:50:16 +0000

    modified

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -21,7 +21,8 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return 'Cannot divide by zero'
-            return num1 / num2
+            elif num2 != 0:
+                return num1 / num2
         case _:
             return "Invalid operation! Please select from \
                 (add, subtract, multiply, divide) only!"

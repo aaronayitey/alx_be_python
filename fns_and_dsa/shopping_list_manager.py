173c012bc1b483db335bdb0f7a1a85bb0825a591
aaronayitey <aaronayitey@gmail.com>
2024-09-28 00:26:53 +0000

    Update user prompts for shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -25,11 +25,11 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter an item to add: ")
+            item = input("Enter item name: ")
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter an item to remove: ")
+            item = input("Enter item name: ")
             if item in shopping_list:
                 shopping_list.remove(item)
             else:

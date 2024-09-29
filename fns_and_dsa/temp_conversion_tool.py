313d033c089843b7ce222839958d83d8fa7021d2
aaronayitey <aaronayitey@gmail.com>
2024-09-29 01:01:37 +0000

    Update variable names

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,15 @@
 # Global conversion factors
-fahrenheit_to_celsius_factor = 5 / 9
-celsius_to_fahrenheit_factor = 9 / 5  
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
-    celsius = (fahrenheit - 32) * fahrenheit_to_celsius_factor
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit."""
-    fahrenheit = celsius * celsius_to_fahrenheit_factor + 32
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
     return fahrenheit
 
 def main():

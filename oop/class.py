cb2a59bd411f0d0c0de0827f9e68ecdf543a4a1b
aaronayitey <aaronayitey@gmail.com>
2024-10-14 23:56:25 +0000

    Initiate class.py

---
+++ oop/class.py
@@ -0,0 +1,18 @@
+class Book:
+    # Constructor (__init__)
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    # Destructor (__del__)
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    # String Representation (__str__)
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    # Official Representation (__repr__)
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
